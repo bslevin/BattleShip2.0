a81bb4e50dcb115bcc855cff2bb5e32a06afa58f
bslevin <blogs82@yahoo.com>
2022-07-22 11:35:46 +0200

    promt for instruction funtion

--- run.py
+++ run.py
@@ -5,7 +5,7 @@ from re import sub
 # Lookup table used to convert from column number to column letter
 COL_LOOKUP = "ABCDEFGH"
 
-# Filter ANSI escape sequences out of any text sent to stdout or stderr on Windows and replace with equiv.
+# Filter ANSI escape sequences out of any text sent to stdout or stderr on Windows and replace with equiv. WIN32 calls
 # Set autoreset to True so that styles are automatically reset after every printed message
 init(autoreset=True)
 
@@ -13,12 +13,10 @@ init(autoreset=True)
 class GameBoard:
 
     def __init__(self, name: str):
-        # Only store first 16 characters of input name to ensure 
-        # the printed nameplate looks nice
+        # Only store first 16 characters of input name to ensure the printed nameplate looks nice
         self.name = name[:16]
 
-        # Players have both a board and public-facing board to 
-        # facilitate information hiding when displaying the board
+        # Players have both a board and public-facing board to facilitate information hiding when displaying the board
         self.board = []
         self.public_board = []
 
@@ -28,12 +26,17 @@ class GameBoard:
 
     # Called to reset the game board, placing empty spaces at all coordinates
     def clear(self):
+        self.board = [[BOARD_WAVE for _ in range(BOARD_SIZE)] for _ in range(BOARD_SIZE)]
+        self.public_board = [[BOARD_WAVE for _ in range(BOARD_SIZE)] for _ in range(BOARD_SIZE)]
+
+    # Called to place ships on the board at random locations
+    def place_ships(self):
         placed_ships = 0
         while placed_ships < NUMBER_OF_SHIPS:
             x = randint(0, BOARD_SIZE - 1)
             y = randint(0, BOARD_SIZE - 1)
 
-           # Skip to the next iteration if a ship has already been placed here
+            # Skip to the next iteration if a ship has already been placed here
             if self.board[x][y] == BOARD_SHIP:
                 continue
 
@@ -41,7 +44,7 @@ class GameBoard:
             placed_ships += 1
 
         self.ships = placed_ships
- 
+
     # Called to display the game board within the terminal
     def display(self, public: bool = False):
         board_name = self.name + "'s Board"
@@ -50,7 +53,7 @@ class GameBoard:
               f"{board_name:^32}\n"
               f"================================")
 
-    # Print the header row of the game board
+        # Print the header row of the game board
         print(BOARD_SEP.join(" ABCDEFGH"))
 
         for i in range(BOARD_SIZE):
@@ -119,12 +122,12 @@ def game_loop(player: GameBoard, comp: GameBoard):
             if not comp.ships:
                 return end_game(player, comp)
 
-            else:
-                print(SHOT_STYLE + f"\nYour shot was a miss!")
+        else:
+            print(SHOT_STYLE + f"\nYour shot was a miss!")
 
-                col, row = chose_random_coordinates(player)
+        col, row = chose_random_coordinates(player)
 
-            # Shoot at randomly chosen coordinates on the Player's game board
+        # Shoot at randomly chosen coordinates on the Player's game board
         if player.shoot_coordinates(col, row):
             print(SHOT_STYLE +
                   f"\n{comp.name} hit your ship at {COL_LOOKUP[col]}{row}. You have {player.ships} ship(s) remaining.")
@@ -148,3 +151,6 @@ def end_game(player: GameBoard, comp: GameBoard):
     # Player Won
     else:
         print(PROMPT_STYLE + VICTORY_TEXT)
+
+# Called at the beginning of the game to check if the player needs instructions
+def prompt_for_instructions():
