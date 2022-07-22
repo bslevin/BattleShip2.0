01e4536ccc36a64202663a493358ed2cfad12c28
bslevin <blogs82@yahoo.com>
2022-07-22 10:01:26 +0200

    board function amend

--- run.py
+++ run.py
@@ -5,10 +5,12 @@
 class GameBoard:
 
     def __init__(self, name: str):
-        # Only store first 16 characters of input name to ensure the printed nameplate looks nice
+        # Only store first 16 characters of input name to ensure 
+        # the printed nameplate looks nice
         self.name = name[:16]
 
-    # Players have both a board and public-facing board to facilitate information hiding when displaying the board
+        # Players have both a board and public-facing board to 
+        # facilitate information hiding when displaying the board
         self.board = []
         self.public_board = []
 
