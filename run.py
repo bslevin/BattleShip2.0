9ad0d7a4b5b17f83382f6c32bf7df81944f8b3e5
bslevin <blogs82@yahoo.com>
2022-07-22 09:56:42 +0200

    GameBoard function for player name input

--- run.py
+++ run.py
@@ -1,1 +1,9 @@
-from colorama import Fore, Style, init 
+
+
+
+# Holds the state of a player and their game board
+class GameBoard:
+
+    def __init__(self, name: str):
+        # Only store first 16 characters of input name to ensure the printed nameplate looks nice
+        self.name = name[:16]
